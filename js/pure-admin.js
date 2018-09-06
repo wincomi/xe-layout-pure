6262320f9af58dd20ab5ee2d9ccffccd4612291b
COMI <admin@wincomi.com>
2018-09-06 16:50:55 +0900

    v1.0.0-beta.1

---
+++ js/pure-admin.js
@@ -0,0 +1,6 @@
+(function($) {
+	"use strict";
+	$(function() {
+		$('div.x').addClass('card-body');
+	});
+})(jQuery);;
---
+++ js/pure.js
@@ -0,0 +1,8 @@
+(function($) {
+	"use strict";
+	$(function() {
+		$('.wfsr:not(#pure-wfsr)').remove();
+
+	});
+
+})(jQuery);;
